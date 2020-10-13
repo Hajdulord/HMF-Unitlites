77ca72c8714704e769195cc7350f4e6437ee7fb3
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2020-10-13 22:26:30 +0200

    Finished abstract Command Pattern

---
+++ Runtime/HMF/Design Patterns/Command Pattern/Command.cs
@@ -0,0 +1,16 @@
+﻿namespace HMF.HMFUtilities.DesignPatterns.CommandPattern
+{
+    public abstract class Command
+    {
+        protected IEntity entity;
+
+        public Command(IEntity entity)
+        {
+            this.entity = entity;
+        }
+
+        public abstract void Execute();
+        public abstract void Undo();
+    }
+}
+
