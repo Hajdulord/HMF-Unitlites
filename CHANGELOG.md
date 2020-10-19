c24575539198fbfb12de9fb005305ec45fca4bfc
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2020-10-19 20:52:45 +0200

    Added the Singleton Pattern

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,17 @@
 ### Added
 - File structure and package.json
 
-## [1.0.1] - 2020.20.13.
+### Removed
+- All unnecessary meta files
+
+## [1.0.1] - 2020.10.13.
 ### Added
 - IEntity 
 - Command for the Command Pattern
 - CommandProcessor that handles all Commands
 
-### Removed
-- All unnecessary meta files
+
+## [1.0.2] - 2020.10.19.
+### Added
+- Singleton Pattern
+- Generic Singleton that derives from MonoBehaviour both persistent and non
---
+++ Runtime/HMF/Design Patterns/Singleton Pattern/SingletonMono.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace HMF.HMFUtilities.DesignPatterns.SingletonPattern
+{
+    // credit : wiki.unity3d.com/index.php?title=Singleton#Generic_Based_Singleton_for_MonoBehaviours
+    public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static bool _shuttingDown = false;
+        private static object _lock = new object();
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_shuttingDown)
+                {
+                    Debug.LogWarning($"[Singleton] Instance |{ typeof(T)}| already destroyed. Returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = FindObjectOfType<T>();
+
+                        if (_instance == null)
+                        {
+                            _instance = new GameObject($"Instance of {typeof(T)}").AddComponent<T>();
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            if (_instance != null)
+                Destroy(gameObject);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _shuttingDown = true;
+        }
+
+
+        private void OnDestroy()
+        {
+            _shuttingDown = true;
+        }
+    }
+}
